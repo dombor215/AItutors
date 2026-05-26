eb4844549fce9dc78ae380d38a4954ebf6e8ddc5
dombor215 <dominik.borovsky123@gmail.com>
2026-05-26 11:41:09 +0200

    Anti copy-paste funcionalities an JSON viewer enhancements
    Anti copy-paste now:
    
    1. replace the pasted text with gibberish (if enabled)
    2. stores a message token which indicates, wheter the message was copied or not
    
    Uses SHA and timestamp to create and decode the token, so it is hard to tamper it within the exported json file.
    
    Viewer now offers also statistics on word counts and typing pace for each conversation and each message, can be showed by clicking "Show metadata".

--- MYP4-mechanics/config_data.js
+++ MYP4-mechanics/config_data.js
@@ -5,6 +5,8 @@ const API_URL = "https://api.poe.com/v1/chat/completions"; // you may adjust thi
 const MODEL_NAME = "GPT-5.3-Instant"; // you may adjust this part based on your prefered AI provider
 const API_FIRST_PART = "sk-poe-XOcVz78f-vmTheTmhvzR-OX6Kpge42xwBtzf"; //fill only part of api and the rest use as a password  to your app
 
+
+const copyPasteProtection = false; // set true to enable random alternation of pasted text 
 // add first message, supported is formatting using Markdown 
 const FIRST_MESSAGE = `Welcome to the AI tutor Maya for physics. Her goal is to guide you through a discussion about the impacts of science on society. Before starting this discussion, remember to:
 
